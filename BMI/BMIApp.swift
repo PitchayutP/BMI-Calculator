2bbcea04e2396def0bb0bfe6df662c9e1963f571
PPsmile <p.fifa2012@gmail.com>
2022-03-22 20:09:14 +0700

    First commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Pitchayut Yimyai on 22/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  BMI
+//
+//  Created by Pitchayut Yimyai on 22/3/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var resultText:String = "BMI Calculator"
+    @State var weight:String = ""
+    @State var height:String = ""
+    var body: some View {
+        VStack {
+            
+            Text(resultText).font(.largeTitle)
+            HStack {
+                Text("Weight")
+                TextField("Kilograms", text: $weight).keyboardType(.numberPad)
+            }.padding()
+            
+            HStack {
+                Text("Height")
+                TextField("Centimeters", text: $height).keyboardType(.numberPad)
+            }.padding()
+            
+            Button(action: {
+                let result = Double(self.weight)! / pow(Double(self.height)! / 100.0, 2)
+                self.resultText = String(result)
+            }) {
+                Text("Calculate")
+            }.padding()
+    
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+//let result = Double(self.weight)! / pow(Double(self.height)! / 100.0, 2)
+//self.resultText = String(result)
+}
